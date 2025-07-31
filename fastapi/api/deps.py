e83dbd970c06013c46e47be018b947869c55ebeb
msayrac <msayracode@gmail.com>
2025-07-31 15:19:55 +0300

    second commit

--- fastapi/api/deps.py
+++ fastapi/api/deps.py
@@ -10,8 +10,9 @@ from .database import SessionLocal
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("AUTH_SECRET_KEY")
-ALGORITHM = os.getenv("AUTH_ALGORITHM")
+SECRET_KEY = os.getenv('AUTH_SECRET_KEY')
+ALGORITHM = os.getenv('AUTH_ALGORITHM')
+
 
 def get_db():
     db = SessionLocal()
@@ -20,26 +21,27 @@ def get_db():
     finally:
         db.close()
 
+
 db_dependency = Annotated[Session, Depends(get_db)]
 
-bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 oauth2_bearer_dependency = Annotated[str, Depends(oauth2_bearer)]
+
 
 async def get_current_user(token: oauth2_bearer_dependency):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get('sub')
         user_id: int = payload.get('id')
-
         if username is None or user_id is None:
-            raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail = "Could not validate user")
-        return {"username": username , "id": user_id}
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
+        return {'username': username, 'id': user_id}
     except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
+
 
 user_dependency = Annotated[dict, Depends(get_current_user)]
-
 
 
 
